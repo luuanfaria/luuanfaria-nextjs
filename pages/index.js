4a9a3777ea058be9c1939508c3d0588b462b0e88
Luan Faria <luan.faria@innovsoftware.com.br>
2022-02-07 11:00:44 -0300

    RM [id].js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,6 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
-// import { getSortedPostsData } from '../lib/posts'
-
-// export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData()
-//   return {
-//     props: {
-//       allPostsData
-//     }
-//   }
-// }
 
 export default function Home() {
   return (
@@ -32,15 +21,6 @@ export default function Home() {
         <p>
           As Full-Stack developer I have experience in React, Typescript, Redux, Hooks, React Native CLI and Expo. Also studying Node, Unit and Integration tests with Jest, Testing Library and Cypress.
         </p>
-
-        {/* <h3>Last blog</h3>
-        {allPostsData.map(({ id, title }) => (
-          <span key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-          </span>
-        ))} */}
       </section>
     </Layout>
   )
